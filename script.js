488c48794f1e72eb57acb3fac359e6951aee32be
tr13ze <13cryss@gmail.com>
2016-04-21 03:42:40 +0200

    add var

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var carousel;
 var slider = document.getElementsByTagName("img");
 var box = document.createElement('picture');
+var style = document.createElement('style');
 var dir = 'http://i.imgur.com/';
 var ext = '.jpg';
 var images = ['HiaZsvV', 'pyiRNAP', 'U9hQk9k'];
 var desc = ['image', 'image', 'image'];
+var i;
 var time = 5000;
 var current = 0;
 
@@ -20,14 +22,13 @@ for (i = 0; i < images.length; i++) {
 }
 
 setInterval(function() {
-    for (var i = 0; i < slider.length; i++) {
+    for (i = 0; i < slider.length; i++) {
         slider[i].style.opacity = 0;
     }
     current = (current != slider.length - 1) ? current + 1 : 0;
     slider[current].style.opacity = 1;
 }, time);
 
-var style = document.createElement('style');
 style.media = 'screen';
 style.type = 'text/css'
 style.innerHTML = "*{ margin: 0;padding: 0;box-sizing: border-box; }html,body{ position:absolute; height:100%; width:100%; }body { overflow: hidden; }picture { position: fixed; top: -50%; left: -50%; width: 200%; height: 200%; }picture img { position: absolute; top: 0; left: 0; right: 0; bottom: 0; margin: auto; min-width: 50%; min-height: 50%; }img { transition: opacity .5s ease-in; }img + img { opacity: 0; }";
